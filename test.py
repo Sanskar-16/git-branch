d429808747a07e718e67d54e02079880d8cff09c
Sanskar <sanskargupta15@gmail.com>
2022-02-21 17:52:21 +0000

    idk at this point

--- test.py
+++ test.py
@@ -1,21 +1,153 @@
-a = 5
+# imports
+from distutils.command.build import build
+import numpy as np
+from tabulate import tabulate
+from itertools import product
 
-b = 6 
+# array keeps hold of the lower matrix, later helps n conversion from binary to decimal
+graph_array = []
+# counter to check for already existing colour vector variations
+pi = 0
+# a list containing all the color vectors
+cv_list = []
+# result holds the value of the multiplication between the matrix and the colour vector
+result = 0
+next_color_vector = 0
+temp_list = []
 
-sum = 0
+matrix = np.array([[0, 0, 1, 0, 1],
+                   [0, 0, 1, 1, 1],
+                   [1, 1, 0, 0, 0],
+                   [0, 1, 0, 0, 0],
+                   [1, 1, 0, 0, 0]])
+rows = 5
+cols = 5
+table = {'Graph number': ['Graph no.', 1],
+         'Number of vertices': ['nov', 1],
+         'starting colour vertex': ['Starting cv', 1],
+         'ending colour vertex': ['Ending cv', 1],
+         'loop': ['Loop', 1],
+         'cycle': ['Cycle', 1]
+         }
 
-sum = a+b
 
-print(sum)
+class GraphList:
 
-c = 77
-d = 11
+    # function to print the lower diagonal
+    def lower(Matrix, row, col):
+        for i in range(0, row):
+            for j in range(0, col):
+                if i == j:
+                    print("0", end=" ")
+                elif i < j:
+                    print(" ", end=" ")
+                else:
+                    print(Matrix[i][j],
+                          end=" ")
+                    # adds the lower matrix the graph_array list as a list of list.
+                    graph_array.append(Matrix[i][j])
 
-e = c - d 
+            print(" ")
 
-print(c)
+    def get_cv_list(self):
+        counter = 0
+        for vector in product([1, -1], repeat=5):
+            # print(roll)
+            cv_list.append(vector)
+            counter = counter + 1
+        print("There are {} number of colour vectors in the cv_list".format(counter))
 
-def summm(a, b):
-    return a+b
+        return cv_list
 
-print(summm(6,5))    
+    # function that calculates the ending colour vector
+    # the cycle it loops on in case the iteration does go into a loop
+    def calculate_length_of_cycle(self):
+        length = len(temp_list) - temp_list.index(next_color_vector)
+        table['ending colour vertex'].append(next_color_vector)
+        table['loop'].append('yes')
+        table['cycle'].append(length)
+
+    # function checking for 0s in the color_vector and replacing them with i of the previous color_vector
+    def check_for_zeroes(x):
+        for k in range(len(x)):
+            if x[k] == 0:
+                x[k] = color_vector[k]
+
+    # try to optimize this function by reducing the time complexity
+
+    def convert_binary_to_decimal(self):
+        strings = [str(integer) for integer in graph_array]
+        graph_string = "".join(strings)
+        # print("The graph number in binary is {}".format(graph_string))
+        graph_num_in_dec = int(graph_string, 2)
+        # print("The converted graph number from binary to decimal is {}".format(graph_num_in_dec))
+        table['Graph number'].append(graph_num_in_dec)
+
+    def append_stuff_to_table(self):
+        # graph table appends here
+        GraphList.convert_binary_to_decimal(self)
+        table['starting colour vertex'].append(cv_list[i])
+        table['Number of vertices'].append(rows)
+
+
+# print the lower diagonal of the matrix multiplication
+print("Lower triangular matrix: ")
+GraphList.lower(matrix, rows, cols)
+print('\n')
+GraphList.get_cv_list(self=)
+print(cv_list)
+
+# not trial code
+for i in range(len(cv_list)):
+    pi = pi + 1
+    print("             Primary iteration {}            ".format(pi))
+    temp_list.clear()
+    count = 0
+    color_vector = np.array(cv_list[i])
+    GraphList.append_stuff_to_table()
+
+    while True:
+        result = np.matmul(matrix, color_vector)
+        next_color_vector = np.sign(result).tolist()
+        if count == 0:
+            temp_list.append(color_vector.tolist())
+        GraphList.check_for_zeroes(next_color_vector)
+
+        if next_color_vector not in temp_list:
+            temp_list.append(next_color_vector)
+            count = count + 1
+            print("             Secondary iteration {}          ".format(count))
+            print("The colour vector for this iteration is {}".format(color_vector))  # negative no -> -1
+            print("The resulting 1x5 matrix is {}".format(result), '\n')
+            color_vector = next_color_vector
+            print("The colour vector for the next iteration is {}".format(next_color_vector), '\n')
+            # add an elif here to check if the program loops, whether it loops over 1 cv or a loop of
+            # different ones to fill up the loop section of the table.
+            # further implement the cycle function to check for the number of cycles it performs.
+
+        else:
+            print("The colour vector iteration repeats here on this row {}".format(count))
+            GraphList.calculate_length_of_cycle()
+            break
+
+    print("The temp_list is {}".format(temp_list))
+
+print(tabulate(table, headers='firstrow', tablefmt='grid'))
+
+# look up into implying a lookup function which checks for the isomorphic graphs.
+# adjacency matrix using a graph library in python
+# looking up properties of graphs like clique and centrality based on the graph's
+
+# list of all the possible graphs with 5 vertices
+# have a function which checks whether they are isomorphic and connected(should reduce the time complexity
+# by skipping all teh non connected and te isomorphic ones
+# Also can have a function which checks whether the iteration of this specific type
+# has already been done before.
+# check if a function can tell us if the graph is connected   just by taking at the adj matrix
+
+# as of now the color vector gets appended first and then it gets checked and calculated further
+# which means that it does not show the initial multiplication. Ex iteration 32
+
+pizza = 69
+bureger = 9
+
